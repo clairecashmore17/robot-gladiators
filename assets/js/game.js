a9cce64ae007ec11d91b3ccf8d3eef40cd3ef9b4
Claire Cashmore <clairecashmore2017@gmail.com>
2022-05-29 11:31:33 -0400

    added functions to js

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,1 +1,17 @@
-window.alert("This is an alert! Javascript is running!");
+// Fight function start
+function fight() {
+    window.alert("The fight has begun!");
+}
+// Fight function end
+// fight();
+
+// storing user input
+var playerName = window.prompt("What is your robot's name?");
+window.alert(playerName);
+console.log(playerName);
+
+console.log("This logs a string, good for leaving yourself a message");
+// This will do math and log 20
+console.log(10+10);
+// what is this? c
+console.log("Our robot's name is " + playerName);
